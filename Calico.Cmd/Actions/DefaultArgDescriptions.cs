3063157c6b5f3d8969c64d31021ff8e9fcf70523
Bas Pennings <baspster@gmail.com>
2017-02-27 22:31:24 +0100

    Formatting

--- Calico.Cmd/Actions/DefaultArgDescriptions.cs
+++ Calico.Cmd/Actions/DefaultArgDescriptions.cs
@@ -2,12 +2,25 @@
 {
     public static class DefaultArgDescriptions
     {
-        public const string ClientId = @"The id of the client that owns the data";
-        public const string PlotId = @"The id of the plot that the data is associated with";
-        public const string FeatureTypeId = @"The id of the feature type that describes the meta data";
-        public const string Shapefile = @"The shapefile to pull data from";
-        public const string TargetDataSet = @"The id of the data set that is going to be refreshed";
-        public const string Top = @"Max number of records to fetch";
-        public const string SpatialReferenceSystem = @"The spatial reference system id (SRID) to use";
+        public const string ClientId =
+            @"The id of the client that owns the data";
+
+        public const string PlotId =
+            @"The id of the plot that the data is associated with";
+
+        public const string FeatureTypeId =
+            @"The id of the feature type that describes the meta data";
+
+        public const string Shapefile =
+            @"The shapefile to pull data from";
+
+        public const string TargetDataSet =
+            @"The id of the data set that is going to be refreshed";
+
+        public const string Top =
+            @"Max number of records to fetch";
+
+        public const string SpatialReferenceSystem =
+            @"The spatial reference system id (SRID) to use";
     }
 }
