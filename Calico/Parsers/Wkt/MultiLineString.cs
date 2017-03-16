62a9f29b9a5ce67c96fa9db5e079e27315d47bee
Bas Pennings <baspster@gmail.com>
2017-03-16 16:08:55 +0100

    Added multilinestring

---
+++ Calico/Parsers/Wkt/MultiLineString.cs
@@ -0,0 +1,31 @@
+﻿// <copyright file="MultiLineString.cs" company="TMG">
+// Copyright (c) TMG. All rights reserved.
+// </copyright>
+
+namespace Calico.Parsers.Wkt
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class MultiLineString : IGeometry
+    {
+        private IEnumerable<LineString> lineStrings;
+
+        public MultiLineString(IEnumerable<LineString> lineStrings)
+        {
+            this.lineStrings = lineStrings;
+        }
+
+        public static string Ident => 
+            nameof(MultiLineString).ToUpperInvariant();
+
+        public GeometryType Type => 
+            throw new NotImplementedException();
+
+        public IEnumerable<Coordinate> Coordinates => 
+            throw new NotImplementedException();
+
+        public IEnumerable<IGeometry> Geometries => 
+            throw new NotImplementedException();
+    }
+}
